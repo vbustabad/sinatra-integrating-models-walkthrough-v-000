6f7a7f77490944c514f43bbf428d5c89da4d6c49
Veronica Bustabad <vbustabad@gmail.com>
2018-07-09 02:42:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :index
   end
@@ -11,4 +12,5 @@ class App < Sinatra::Base
 
     erb :results
   end
+
 end
